cdee695d47191dec839764e704121a080b352743
wouter-v75 <wouter@wvsailing.co.uk>
2025-08-29 19:31:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Tip: if you use `vercel dev` for local API functions, they default to port 3000.
-// If you want Vite (5173) to proxy /api/* to that, uncomment the proxy section.
+
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    // proxy: { '/api': 'http://localhost:3000' }
-  }
+plugins: [vue()]
 })
