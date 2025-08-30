5a5e8edd2ba88d73c4a3e75a140a36d970ee56ab
wouter-v75 <wouter@wvsailing.co.uk>
2025-08-30 06:19:27 +0300

    Create Performance.vue

---
+++ src/views/Performance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+    <h1>Performance Dashboard</h1>
+    <p>This page will show performance data (VMG, polars, analytics).</p>
+
+    <ul>
+      <li>VMG Upwind: -- knots (stub)</li>
+      <li>VMG Downwind: -- knots (stub)</li>
+      <li>Overall Efficiency: -- % (stub)</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.page {
+  padding: 1rem;
+}
+</style>
