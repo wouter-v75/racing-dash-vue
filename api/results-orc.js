79a556be2f799bc57836a4cc89cf5d5e32499574
wouter-v75 <wouter@wvsailing.co.uk>
2025-08-30 06:15:58 +0300

    Create results-orc.js

---
+++ api/results-orc.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') return res.status(200).end()
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' })
+
+  const { eventId, classId, raceId } = req.body
+  // TODO: implement ORC scraping like your working parser
+  return res.json({ ok: true, eventId, classId, raceId })
+}
